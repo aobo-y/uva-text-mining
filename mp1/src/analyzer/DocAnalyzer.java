07f1622fd10a09237dd3d24bc731c15129de2415
Aobo Yang <yangaobo@gmail.com>
2019-02-13 14:37:19 -0500

    mp1: normalization

--- mp1/src/analyzer/DocAnalyzer.java
+++ mp1/src/analyzer/DocAnalyzer.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package analyzer;
 
@@ -12,6 +12,9 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.regex.Pattern;
+import java.util.Arrays;
+import java.util.Set;
 
 import org.tartarus.snowball.SnowballStemmer;
 import org.tartarus.snowball.ext.englishStemmer;
@@ -30,35 +33,35 @@ import structures.Token;
 
 /**
  * @author hongning
- * Sample codes for demonstrating OpenNLP package usage 
- * NOTE: the code here is only for demonstration purpose, 
+ * Sample codes for demonstrating OpenNLP package usage
+ * NOTE: the code here is only for demonstration purpose,
  * please revise it accordingly to maximize your implementation's efficiency!
  */
 public class DocAnalyzer {
 	//N-gram to be created
 	int m_N;
-	
+
 	//a list of stopwords
 	HashSet<String> m_stopwords;
-	
+
 	//you can store the loaded reviews in this arraylist for further processing
 	ArrayList<Post> m_reviews;
-	
+
 	//you might need something like this to store the counting statistics for validating Zipf's and computing IDF
-	HashMap<String, Token> m_stats;	
-	
+	HashMap<String, Token> m_stats;
+
 	//we have also provided a sample implementation of language model in src.structures.LanguageModel
 	Tokenizer m_tokenizer;
-	
+
 	//this structure is for language modeling
 	LanguageModel m_langModel;
-	
+
 	public DocAnalyzer(String tokenModel, int N) throws InvalidFormatException, FileNotFoundException, IOException {
 		m_N = N;
 		m_reviews = new ArrayList<Post>();
 		m_tokenizer = new TokenizerME(new TokenizerModel(new FileInputStream(tokenModel)));
 	}
-	
+
 	//sample code for loading a list of stopwords from file
 	//you can manually modify the stopword file to include your newly selected words
 	public void LoadStopwords(String filename) {
@@ -79,55 +82,62 @@ public class DocAnalyzer {
 			System.err.format("[Error]Failed to open file %s!!", filename);
 		}
 	}
-	
-	public void analyzeDocument(JSONObject json) {		
+
+	public void analyzeDocument(JSONObject json) {
 		try {
 			JSONArray jarray = json.getJSONArray("Reviews");
 			for(int i=0; i<jarray.length(); i++) {
 				Post review = new Post(jarray.getJSONObject(i));
-				
+
 				String[] tokens = Tokenize(review.getContent());
-				review.setTokens(tokens);
-				
+
 				/**
 				 * HINT: perform necessary text processing here based on the tokenization results
 				 * e.g., tokens -> normalization -> stemming -> N-gram -> stopword removal -> to vector
-				 * The Post class has defined a "HashMap<String, Token> m_vector" field to hold the vector representation 
+				 * The Post class has defined a "HashMap<String, Token> m_vector" field to hold the vector representation
 				 * For efficiency purpose, you can accumulate a term's DF here as well
 				 */
-				
-				
+
+				tokens = Arrays.stream(tokens)
+					.map(s -> Normalization(s))
+					.map(s -> s.trim())
+					.filter(s -> !s.isEmpty())
+					.toArray(String[]::new);
+
+				review.setTokens(tokens);
+
+
 				m_reviews.add(review);
 			}
 		} catch (JSONException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void createLanguageModel() {
 		m_langModel = new LanguageModel(m_N, m_stats.size());
-		
+
 		for(Post review:m_reviews) {
 			String[] tokens = Tokenize(review.getContent());
 			/**
-			 * HINT: essentially you will perform very similar operations as what you have done in analyzeDocument() 
+			 * HINT: essentially you will perform very similar operations as what you have done in analyzeDocument()
 			 * Now you should properly update the counts in LanguageModel structure such that we can perform maximum likelihood estimation on it
 			 */
 		}
 	}
-	
+
 	//sample code for loading a json file
 	public JSONObject LoadJson(String filename) {
 		try {
 			BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(filename), "UTF-8"));
 			StringBuffer buffer = new StringBuffer(1024);
 			String line;
-			
+
 			while((line=reader.readLine())!=null) {
 				buffer.append(line);
 			}
 			reader.close();
-			
+
 			return new JSONObject(buffer.toString());
 		} catch (IOException e) {
 			System.err.format("[Error]Failed to open file %s!", filename);
@@ -139,8 +149,8 @@ public class DocAnalyzer {
 			return null;
 		}
 	}
-	
-	// sample code for demonstrating how to recursively load files in a directory 
+
+	// sample code for demonstrating how to recursively load files in a directory
 	public void LoadDirectory(String folder, String suffix) {
 		File dir = new File(folder);
 		int size = m_reviews.size();
@@ -163,7 +173,7 @@ public class DocAnalyzer {
 		else
 			return token;
 	}
-	
+
 	//sample code for demonstrating how to use Porter stemmer
 	public String PorterStemming(String token) {
 		porterStemmer stemmer = new porterStemmer();
@@ -173,42 +183,90 @@ public class DocAnalyzer {
 		else
 			return token;
 	}
-	
+
 	//sample code for demonstrating how to perform text normalization
 	//you should implement your own normalization procedure here
 	public String Normalization(String token) {
 		// remove all non-word characters
 		// please change this to removing all English punctuation
-		token = token.replaceAll("\\W+", ""); 
-		
+
+		// after tokenization, the actual punctuation should only appear in the either ends
+		token = token.replaceAll("(^\\p{Punct}+|\\p{Punct}+$)", "");
+
 		// convert to lower case
-		token = token.toLowerCase(); 
-		
+		token = token.toLowerCase();
+
 		// add a line to recognize integers and doubles via regular expression
+
+		// take care of formatted number with comma: 3,000.01
+		Pattern numberPtn = Pattern.compile("\\d+(,\\d{3})*(\\.\\d+)?");
+		Boolean isNumber = numberPtn.matcher(token).matches();
+
 		// and convert the recognized integers and doubles to a special symbol "NUM"
-		
+		if (isNumber) {
+			token = "NUM";
+		}
+
 		return token;
 	}
-	
+
 	String[] Tokenize(String text) {
 		return m_tokenizer.tokenize(text);
 	}
-	
+
 	public void TokenizerDemon(String text) {
 		System.out.format("Token\tNormalization\tSnonball Stemmer\tPorter Stemmer\n");
-		for(String token:m_tokenizer.tokenize(text)){
+		for(String token: m_tokenizer.tokenize(text)){
 			System.out.format("%s\t%s\t%s\t%s\n", token, Normalization(token), SnowballStemming(token), PorterStemming(token));
-		}		
+		}
 	}
-	
-	public static void main(String[] args) throws InvalidFormatException, FileNotFoundException, IOException {		
+
+	private void zipfLaw() {
+		LoadDirectory("./yelp/train", ".json");
+		LoadDirectory("./yelp/test", ".json");
+
+		HashMap<String, Integer> ttf = new HashMap<>();
+		HashMap<String, Integer> df = new HashMap<>();
+
+		for (Post review: m_reviews) {
+			for (String token: review.getTokens()) {
+				if (ttf.containsKey(token)) {
+					ttf.put(token, ttf.get(token) + 1);
+				} else {
+					ttf.put(token, 1);
+				}
+			}
+
+			Set<String> tokenSet = new HashSet<>(Arrays.asList(review.getTokens()));
+			for (String token: tokenSet) {
+				if (df.containsKey(token)) {
+					df.put(token, df.get(token) + 1);
+				} else {
+					df.put(token, 1);
+				}
+			}
+		}
+
+		System.out.println("[ttf]");
+		for (String token: ttf.keySet()) {
+			System.out.println(token + "\t" + ttf.get(token));
+		}
+
+		System.out.println("[df]");
+		for (String token: df.keySet()) {
+			System.out.println(token + "\t" + df.get(token));
+		}
+	}
+
+	public static void main(String[] args) throws InvalidFormatException, FileNotFoundException, IOException {
 		DocAnalyzer analyzer = new DocAnalyzer("./data/Model/en-token.bin", 2);
-		
+
 		//code for demonstrating tokenization and stemming
-		analyzer.TokenizerDemon("I've practiced for 30 years in pediatrics, and I've never seen anything quite like this.");
-			
+		// analyzer.TokenizerDemon("I've practiced for 30 30,000 30.1 002 2.2.2 3.3a 3,0,1,0.001 years in pediatrics, I've never seen anything quite like this.");
+
 		//entry point to deal with a collection of documents
-		analyzer.LoadDirectory("./Data/yelp/train", ".json");
+
+		analyzer.zipfLaw();
 	}
 
 }
